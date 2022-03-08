db595be193252d5dbe04bf5c25e0b10cc0ebb81f
Himenon <k.himeno314@gmail.com>
2022-03-08 20:50:18 +0900

    chore: update

--- scripts/fetchSchema.ts
+++ scripts/fetchSchema.ts
@@ -4,12 +4,14 @@ import fetch from "node-fetch";
 import * as Shell from "./tools/shell";
 import rimraf from "rimraf";
 import { versions, outputDir } from "./config";
+import * as yaml from "js-yaml";
 
-export const getKubernetesSwaggerSchema = async (version: string): Promise<any> => {
+export const getSwaggerSchema = async (version: string): Promise<any> => {
   const url = `https://docs.docker.com/engine/api/${version}.yaml`;
   try {
     const res = await fetch(url);
-    return res.json();
+    const yamlText = await res.text();
+    return yaml.load(yamlText);
   } catch (error) {
     throw error as any;
   }
@@ -25,10 +27,10 @@ const main = async () => {
   rimraf.sync(outputDir);
   fs.mkdirSync(outputDir, { recursive: true });
   const tasks = versions.map(async version => {
-    const result = await getKubernetesSwaggerSchema(version);
+    const swaggerShema = await getSwaggerSchema(version);
     const swaggerFilename = path.join(tempDir, `swagger-${version}.json`);
     const openapiFilename = path.join(outputDir, `openapi-${version}.json`);
-    fs.writeFileSync(swaggerFilename, JSON.stringify(result, null, 2), { encoding: "utf-8" });
+    fs.writeFileSync(swaggerFilename, JSON.stringify(swaggerShema, null, 2), { encoding: "utf-8" });
     await convert(swaggerFilename, openapiFilename);
   });
   await Promise.all(tasks);
